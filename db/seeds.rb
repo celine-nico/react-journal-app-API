99d692bf892f36a8a99f02eb9ac76d771666bdce
Cel <celinenico22@gmail.com>
2021-07-26 13:24:10 -0400

    fixed synatx errors in seed file, cleared seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Journal.create({
-    date: "3-12-2020",
-    entry: "Today is my first day in FLatiron School! I'm so excited to learn about software and build new skills.
+    date: "2-22-2021",
+    entry: "Today is my first day in Flatiron School! I'm so excited to learn about software engineering and build new skills.
     Hit me with your best shot!",
     emotion: "Content"
 })
